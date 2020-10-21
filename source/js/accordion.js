6ee718a0fabb9dec1ae8c1b6648f0f70ae0ee04c
Мария <slinkywizard@mail.ru>
2020-10-21 23:08:32 +0300

    Верстка блоков Хочу поехать, Как поехать, Отзывы

---
+++ source/js/accordion.js
@@ -0,0 +1,24 @@
+var accordionItemTrigger = document.querySelectorAll(".accordion__item-trigger");
+var accordionItem = document.querySelectorAll(".accordion__item");
+var arrowItems = document.querySelectorAll(".accordion__item-svg");
+
+accordionItemTrigger.forEach(function (item) {
+  item.addEventListener("click", () => {
+    arrowItems.forEach(function (item) {
+      item.style.transform = "none";
+    })
+
+    const parent = item.parentNode;
+    var arrowItem = item.querySelector(".accordion__item-svg");
+    arrowItem.style.transform = "rotate(360deg)";
+    if (parent.classList.contains("accordion__item--active")) {
+      parent.classList.remove("accordion__item--active");
+    } else {
+      accordionItem.forEach((child) => child.classList.remove("accordion__item--active"))
+
+      parent.classList.add("accordion__item--active");
+      arrowItem.style.transform = "rotate(180deg)";
+    }
+  })
+});
+
